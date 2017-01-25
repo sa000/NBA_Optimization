ef89a555068549d615ad65a787ae30d20da8f0c0
sa000 <alamsakib@gmail.com>
2017-01-24 20:43:02 -0600

    tr

--- scripts/transactions.py
+++ scripts/transactions.py
@@ -8,12 +8,12 @@ import csv
 
 def get_transactions():
 	projections=pd.read_csv('../Projections/Projections.csv')
-	headers=['Name', 'Dropped', 'Added', 'Total', 'Date']
+	headers=['Name', 'Team', 'Position', 'Dropped', 'Added', 'Total', 'Date']
 	target=open('../Projections/transactions.csv', 'w')
 	csvwriter=csv.writer(target)
 	csvwriter.writerow(headers)
 	dates=projections.Date.unique()
-	for date in dates[0:1]:
+	for date in dates:
 		print date
 		url='https://basketball.fantasysports.yahoo.com/nba/buzzindex?sort=BI_S&src=combined&bimtab=A&pos=ALL&date=%s' %(date)
 		print url
@@ -22,14 +22,18 @@ def get_transactions():
 		table=soup.find("table", "Tst-table Table")
 		for row in table.findAll('tr')[1:]:
 			player=row.findAll('td')
-			for link in player[0].findAll('a', href=True):
-				if 'news' not in link['href'] and 'players' in link['href']:
-					p_url=link['href']
-					print p_url
-					p_page=requests.get(p_url)
-					p_soup=BeautifulSoup(p_page.text,"lxml")
-					name=p_soup.h1.text
-			data=[name, player[1].text, player[2].text, player[4].text, date]
+			for entry in player[0].findAll(text=True):
+				name=player[0].findAll(text=True)[6]
+				team=player[0].findAll(text=True)[8].split('-')[0].upper()
+				pos=player[0].findAll(text=True)[8].split('-')[1].upper()
+			data=[name, team, pos, player[1].text, player[2].text, player[4].text, date]
 			csvwriter.writerow(data)
 	target.close()
-get_transactions()
+
+def extend_name():
+	projections=pd.read_csv('../Projections/Projections.csv')
+	transactions=pd.read_csv('../Projections/transactions.csv')
+	for index, row in transactions.iterrows():
+		print row
+# get_transactions()
+extend_name()
